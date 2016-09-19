42ff7ef3fced69794794f4b4a6f69a7643a48e11
Dan <gwflagged@gmail.com>
2016-09-19 19:53:36 -0400

    Added a feature to set directory

--- CompileAndRun.py
+++ CompileAndRun.py
@@ -1,18 +1,31 @@
 import subprocess
 import sys
+import os
 
 # This python file simply compiles and runs an input file, cutting down on the time it takes to use the commandline.
 
 def compile_java(java_file):
     print("Compiling and running file: "+java_file)
     cmd = "javac " + java_file
-    proc = subprocess.Popen(cmd, shell=True)
+    proc = subprocess.Popen("exec "+cmd, shell=True)
+    # output = proc.communicate()[0]
+    # if(proc.returncode != 0):
+        # print("There was an error while compiling the file!")
+        # sys.exit()
     proc.wait()
-
 def run_java(java_file):
     cmd = "java " + java_file
     proc = subprocess.Popen(cmd, shell=True)
     proc.wait()
+
+directory = input("Set your directory (leave blank to use the current one): ")
+
+if(directory == ""):
+    print("Using directory '"+os.getcwd()+"'")
+    directory = os.getcwd()
+else:
+    print("Using directory '"+os.getcwd()+"/"+directory+"'")
+    os.chdir(os.getcwd()+"/"+directory)
 
 while(1 == 1):
     java_file = input("Filename: ")
